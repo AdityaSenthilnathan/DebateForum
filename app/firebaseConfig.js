511df1d37e589aa2c39402eebd30f419beaee4d4
Cryptic <66099566+AdityaSenthilnathan@users.noreply.github.com>
2024-11-26 15:43:49 -0800

    Update firebaseConfig.js

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -2,26 +2,20 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { GoogleAuthProvider, getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"; // Import Firestore
 
-const NEXT_PUBLIC_FIREBASE_API_KEY = process.env.NEXT_PUBLIC_FIREBASE_API_KEY;
-console.log("NEXT_PUBLIC_FIREBASE_API_KEY:", NEXT_PUBLIC_FIREBASE_API_KEY); // Add this line to verify the environment variable
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "debateforum-3be19.firebaseapp.com",
   projectId: "debateforum-3be19",
-  storageBucket: "debateforum-3be19.appspot.com", // Updated this to match standard naming
+  storageBucket: "debateforum-3be19.appspot.com",
   messagingSenderId: "24147286853",
   appId: "1:24147286853:web:192573977b5b38d4d0fc2c",
   measurementId: "G-X1SSYP2RTE"
 };
 
 // Initialize Firebase
-const firestoreApp = getApps().length
-  ? getApp()
-  : initializeApp(firebaseConfig);
+const firestoreApp = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const googleAuthProvider = new GoogleAuthProvider();
 const auth = getAuth(firestoreApp);
 const db = getFirestore(firestoreApp); // Initialize Firestore instance
 const googleProvider = new GoogleAuthProvider(); // Initialize as googleProvider
-export { auth, googleAuthProvider, db, googleProvider}; // Export db along with auth and provider
+export { auth, googleAuthProvider, db, googleProvider }; // Export db along with auth and provider
