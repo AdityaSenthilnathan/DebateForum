fb7079827968d2ad9818f9235427673266d0539e
Cryptic <66099566+AdityaSenthilnathan@users.noreply.github.com>
2024-11-28 15:01:56 -0800

    Update firebaseConfig.ts

--- app/firebaseConfig.ts
+++ app/firebaseConfig.ts
@@ -3,6 +3,7 @@ import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase
 import { getFirestore, doc, setDoc } from 'firebase/firestore';
 import { initializeApp } from 'firebase/app';
 import { GoogleAuthProvider } from "firebase/auth";
+import { getApp, getApps } from "firebase/app";
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: "debateforum-3be19.firebaseapp.com",
@@ -12,12 +13,12 @@ const firebaseConfig = {
     appId: "1:24147286853:web:192573977b5b38d4d0fc2c",
     measurementId: "G-X1SSYP2RTE"
 };
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+const firestoreApp = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const googleAuthProvider = new GoogleAuthProvider();
-export { auth, db, googleAuthProvider };
+const auth = getAuth(firestoreApp);
+const db = getFirestore(firestoreApp); // Initialize Firestore instance
+const googleProvider = new GoogleAuthProvider(); // Initialize as googleProvider
+export { auth, googleAuthProvider, db, googleProvider }; // Export db along with auth and provider
 
 export const signUpUser = async (email: string, password: string, displayName: string) => {
   try {
